3d1abd9f5d98f8c5c516951d5dd6b215d786168b
y-u-y-a <yuya@yys-MacBook-Air.local>
2019-03-11 15:19:43 +0900

    git

--- config/routes.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.routes.draw do
-  root 'items#index'
-  devise_for :users
-  resources :items,only: [:index,:new]
-end
--- db/migrate/20190308063933_create_item_images.rb
+++
@@ -1,9 +0,0 @@
-class CreateItemImages < ActiveRecord::Migration[5.0]
-  def change
-    create_table :item_images do |t|
-      t.string      :image
-      t.integer     :item_id
-      t.timestamps
-    end
-  end
-end
