466c424f4bead5540daa4511570d01028f7aa221
ide-ryunosuke <r.i1996423@gmail.com>
2019-03-11 15:17:09 +0900

    Gitignore

--- config/routes.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.routes.draw do
-  root 'items#index'
-  devise_for :users
-  resources :items,only: [:index,:new]
-  resources :users,only: [:index,:show]
-end
--- vendor/bundle/ruby/2.3.0/gems/fog-aws-3.3.0/lib/fog/aws/version.rb
+++
@@ -1,5 +0,0 @@
-module Fog
-  module AWS
-    VERSION = "3.3.0"
-  end
-end
