9f8f9164bcf0a20cddd46eef74b83ee8e868044b
tsurutadesu <k.tsuruta.1108@gmail.com>
2019-03-12 14:54:10 +0900

    make logout view

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   root 'items#index'
   devise_for :users
   resources :users do
-    get :signup, on: :collection
-    get :login     , on: :collection
-    get :payment_method,   on: :collection
+    get :signup,            on: :collection
+    get :login,             on: :collection
+    get :logout,            on: :collection
+    get :payment_method,    on: :collection
     get :card_registration, on: :collection
   end
   resources :items,only: [:index,:new]
