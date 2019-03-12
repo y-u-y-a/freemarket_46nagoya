c4eb034cbbd1bcf667bb65d8350b83433ee1ecba
y-u-y-a <yuya@yys-MacBook-Air.local>
2019-03-12 12:59:43 +0900

    conflict

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,19 +5,19 @@ class UsersController < ApplicationController
   def show
   end
 
-<<<<<<< HEAD
+
 
   def signup
   end
 
   def login
   end
-=======
+
   def payment_method
   end
 
   def card_registration
   end
 
->>>>>>> tsurutadesu/master
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,10 @@ Rails.application.routes.draw do
   root 'items#index'
   devise_for :users
   resources :users do
-<<<<<<< HEAD
     get :signup, on: :collection
     get :login     , on: :collection
-=======
     get :payment_method,   on: :collection
     get :card_registration, on: :collection
->>>>>>> tsurutadesu/master
   end
   resources :items,only: [:index,:new]
   resources :users,only: [:index,:show]
