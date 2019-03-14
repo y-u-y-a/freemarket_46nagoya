35ab765214c15dbb3675b9ab9b3cf458ed006ca7
ide-ryunosuke <r.i1996423@gmail.com>
2019-03-14 20:05:49 +0900

    Edit-controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,9 +8,9 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.create(item_params)
-    @item_image = Item.new(item_image_params)
-    @item_image.save
+    @item = Item.new(item_params)
+    binding.pry
+    @item.save
     redirect_to root_path(@item)
   end
 
@@ -24,9 +24,5 @@ class ItemsController < ApplicationController
   private
     def item_params
       params.permit(:name, :explain, :category_id, :state, :postage, :region, :shipping_date, :price)
-    end
-
-    def item_image_params
-      params.permit(:image)
     end
 end
