d0416eb16bb2a8eecc24d94990566264b9e61845
ide-ryunosuke <r.i1996423@gmail.com>
2019-03-15 12:28:44 +0900

    Revert "edit-models"
    This reverts commit f2950cb766afef439ec63aec44667b2ddf711a64.

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,2 @@
 class Item < ApplicationRecord
-  has_many :item_images
-  accepts_nested_attributes_for :item_images
 end
--- app/models/item_image.rb
+++ app/models/item_image.rb
@@ -1,3 +1,2 @@
 class ItemImage < ApplicationRecord
-  belongs_to :item
 end
