27c6991160f6b114bafe3a09e353f1ddd49a428b
y-u-y-a <yuya@yys-MacBook-Air.local>
2019-03-17 15:29:03 +0900

    show products list

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,16 @@
 class ItemsController < ApplicationController
+
+
   def index
+    item = Item.order("created_at DESC")
+    @lady_items = item.where(category_id: "1").first(4)
+    @man_items = item.where(category_id: "2").first(4)
+    @kids_items = item.where(category_id: "3").first(4)
+    @cosmetic_items = item.where(category_id: "6").first(4)
+    @chanel_items = item.where(brand_id: "1").first(4)
+    @nike_items = item.where(brand_id: "2").first(4)
+    @vuitton_items = item.where(brand_id: "3").first(4)
+    @supreme_items = item.where(brand_id: "4").first(4)
   end
 
   def new
@@ -8,7 +19,7 @@ class ItemsController < ApplicationController
 
   def show
   end
-  
+
   def buy
 
   end
