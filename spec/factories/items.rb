480661f79b4883169ca0e96daeac8227b20d9eb6
tsurutadesu <k.tsuruta.1108@gmail.com>
2019-03-19 17:17:15 +0900

    delete conflict

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -13,18 +13,17 @@ FactoryBot.define do
     category_id           {"1"}
     created_at            {Faker::Time.between(99.days.ago, Time.now, :all)}     # 100日間の間ででランダム生成
   end
-end
+
 # :itemを複数形の:itemsにすると、NameError：uninitialized const Itemというエラーが出る
-=======
-  factory :item do
-    name {"apple"}
-    price {1000}
-    explain {"aaa"}
-    state{1}
-    postage {1}
-    shipping_date {1}
-    shipping_way {1}
-    user_id {1}
-    user
-  end
+  # factory :item do
+  #   name {"apple"}
+  #   price {1000}
+  #   explain {"aaa"}
+  #   state{1}
+  #   postage {1}
+  #   shipping_date {1}
+  #   shipping_way {1}
+  #   user_id {1}
+  #   user
+  # end
 end
