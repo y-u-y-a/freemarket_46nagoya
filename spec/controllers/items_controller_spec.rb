9335f7d1ca2adbb283c9814245e14defe026596a
ide-ryunosuke <r.i1996423@gmail.com>
2019-03-19 13:30:27 +0900

    Controller-test

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -16,4 +16,10 @@ RSpec.describe ItemsController, type: :controller do
     end
   end
 
+  describe "POST #create" do
+    it "returns http success" do
+      post :create
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
