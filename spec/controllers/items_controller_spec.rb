05b9b90cbc6d06a9102202e1d93624b4d43b5a2e
y-u-y-a <yuya@yys-MacBook-Air.local>
2019-03-19 12:32:17 +0900

    complete test

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -5,14 +5,14 @@ RSpec.describe ItemsController, type: :controller do
   describe "GET #index" do
 
     it "indexアクション内で定義したインスタンス変数が期待したものになるか？" do
+      item = FactoryBot.create_list(:item, 4)
       # createなので、FactoryBotで定義したデータをtest環境データベースに3つ保存して、変数itemにも代入する
-      items = []
-      items = FactoryBot.create_list(:items, 100)
-      # lady_items = items.where(category_id: "1").first(4)
+      lady_items = item.sort{ |a, b| b.created_at <=> a.created_at }
+      # 擬似的にindexアクションを動かす
       get :index
-      # assignsメソッドの引数はFactoryBotで定義している変数を指す
-      #matchの引数はこのエクスペクテーション内で定義した変数で、さっき保存されたデータを使ってデータが格納される
-      expect(assigns(:items)).to match(items.sort{ |a, b| b.created_at <=> a.created_at })
+      # assignsメソッドの引数はコントローラで定義した@lady_itemsを指す
+      # matchの引数はこのエクスペクテーション内で定義した変数で、さっき保存されたデータを使ってデータが格納される
+      expect(assigns(:lady_items)).to match(lady_items)
     end
 
     it "returns http success" do
