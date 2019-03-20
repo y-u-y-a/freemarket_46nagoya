604ebf2b792c559654a6f932d7fdea2def9fadf5
y-u-y-a <yuya@yys-MacBook-Air.local>
2019-03-20 17:27:28 +0900

    change file

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
     :registrations => 'users/registrations',
     :sessions => 'users/sessions'
   }
+  devise_scope :user do
+    get 'phone_number' => 'users/registrations#phone_number'
+    get 'address' => 'users/registrations#address'
+    get 'payment_method' => 'users/registrations#payment_method'
+  end
   resources :users do
     get :to_signup,            on: :collection
     get :logout,               on: :collection
