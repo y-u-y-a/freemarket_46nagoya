e3f3791166533de6616a6afa152e074169cfd096
y-u-y-a <yuya@yys-MacBook-Air.local>
2019-03-21 20:20:44 +0900

    usersテーブルにphone_numberカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190320040622) do
+ActiveRecord::Schema.define(version: 20190321111910) do
 
   create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id"
@@ -56,10 +56,9 @@ ActiveRecord::Schema.define(version: 20190320040622) do
     t.integer  "brand_id"
     t.datetime "created_at",                                        null: false
     t.datetime "updated_at",                                        null: false
-    t.integer  "user_id"
+    t.integer  "shipping_way",                          default: 0
     t.integer  "buyer_id"
     t.integer  "business_stats"
-    t.integer  "shipping_way",                          default: 0
     t.integer  "category_id"
     t.integer  "child_category_id"
     t.integer  "grand_child_category_id"
@@ -81,8 +80,7 @@ ActiveRecord::Schema.define(version: 20190320040622) do
     t.datetime "remember_created_at"
     t.datetime "created_at",                                        null: false
     t.datetime "updated_at",                                        null: false
-    t.string   "card_token"
-    t.string   "customer_id"
+    t.integer  "phone_number"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
