f3c87ae3cd6df78b8551eaae8b807b34127a3010
ide-ryunosuke <r.i1996423@gmail.com>
2019-03-22 16:55:56 +0900

    Edit-file

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,4 @@ Rails.application.routes.draw do
   resources :items do
     get :buy, on: :collection
   end
-  resources :items,only: [:index,:new,:create,:show,:edit]
-  resources :users,only: [:index,:show]
 end
