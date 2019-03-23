d3a40c1e9deef93e46de18182ebe8da9617a0c0b
y-u-y-a <yuya@yys-MacBook-Air.local>
2019-03-23 14:43:26 +0900

    bundle install

--- db/schema.rb
+++ db/schema.rb
@@ -80,6 +80,8 @@ ActiveRecord::Schema.define(version: 20190322100445) do
     t.datetime "remember_created_at"
     t.datetime "created_at",                                        null: false
     t.datetime "updated_at",                                        null: false
+    t.string   "card_token"
+    t.string   "customer_id"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
