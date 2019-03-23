4d2c55d097e0202f2559c2ee2fe4028864464370
tsurutadesu <k.tsuruta.1108@gmail.com>
2019-03-23 19:48:42 +0900

    delete

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,6 @@ class User < ApplicationRecord
         uid:      auth.uid,
         provider: auth.provider,
         nickname:     auth.info.name,
-        # email:        auth.info.email,
         email:    User.dummy_email(auth),
         password: Devise.friendly_token[0, 20]
         )
