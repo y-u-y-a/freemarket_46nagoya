e3e8a7f948de1bd49bc43bb9292ac669aeee5ab8
tsurutadesu <k.tsuruta.1108@gmail.com>
2019-03-23 12:22:31 +0900

    Merge remote-tracking branch 'origin/master'

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190320040622) do
+ActiveRecord::Schema.define(version: 20190322082500) do
 
   create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id"
@@ -81,6 +81,11 @@ ActiveRecord::Schema.define(version: 20190320040622) do
     t.datetime "remember_created_at"
     t.datetime "created_at",                                        null: false
     t.datetime "updated_at",                                        null: false
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "card_token"
+    t.string   "customer_id"
+
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
