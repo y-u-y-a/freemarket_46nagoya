e94225b75ef166fca213e7c0b606f9b6cecc1172
tsurutadesu <k.tsuruta.1108@gmail.com>
2019-03-23 14:30:46 +0900

    comment out

--- db/migrate/20190323052516_add_column_to_items.rb
+++ db/migrate/20190323052516_add_column_to_items.rb
@@ -1,6 +1,6 @@
 class AddColumnToItems < ActiveRecord::Migration[5.0]
   def change
-    remove_column :items, :user_id, :integer
+    # remove_column :items, :user_id, :integer
 
     add_column :items, :user_id, :integer
   end
