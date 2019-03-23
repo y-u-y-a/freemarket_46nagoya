e2a87106e0577c60a955c7fbcb27847528c8f4d0
tsurutadesu <k.tsuruta.1108@gmail.com>
2019-03-23 15:43:44 +0900

    make edit & show item

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,13 +8,6 @@ class ApplicationController < ActionController::Base
     end
 
   private
-
-  def production?
-    Rails.env.production?
-  end
-
-  private
-
   def production?
     Rails.env.production?
   end
