f1f93d141c83c367081fe8a252e2b6d2dc58e041
y-u-y-a <yuya@yys-MacBook-Air.local>
2019-03-29 10:49:49 +0900

    ブランドの一覧表示完了

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -1,13 +1,14 @@
 class BrandsController < ApplicationController
 
   before_action :category_in_brand, only: :show
+  before_action :set_search,        only: :show
 
   def show
     # カテゴリーが選択される
     @category = Category.find(params[:id])
-    # 中間テーブルから関連したものを抽出する(カテゴリーで検索)
-    @categoryBrands = @category.category_brands
-    # ブランドのイニシャルを抽出する
-    @brands = Brand.where(id: @categoryBrands.brand_id)
+    # カテゴリーが持つブランドのレコードを抽出
+    @Brands = @category.brands
+    # 取得したレコードからinitialカラムの中だけを取得して配列で返す
+    @initials = @Brands.pluck(:initial).uniq
   end
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -102,7 +102,12 @@ class ItemsController < ApplicationController
   end
 
   def all_brands_show
-    @bike_initial = ["ア","オ","カ","シ","ス","ハ","ヒ","ホ","ヤ","ユ"]
+    # カテゴリーが選択される
+    @category = Category.find(1)
+    # カテゴリーが持つブランドのレコードを抽出
+    @Brands = @category.brands
+    # 取得したレコードからinitialカラムの中だけを取得して配列で返す
+    @initials = @Brands.pluck(:initial).uniq
   end
 
   def all_categories_show
